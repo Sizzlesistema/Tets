ace68d963ab8cf31a808e42daba03cfe5d95ed67
Sizzlesistema <sizzlesistema@gmail.com>
2025-04-28 00:23:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.getElementById('reveal-btn').addEventListener('click', function() {
     const secretCode = document.getElementById('secret-code').value;
     
-    if (secretCode === 'pituchinha') {
+    if (secretCode === 'Pituchinha') {
       // Se o código estiver correto, mostra a mensagem
       document.getElementById('message').classList.remove('hidden');
     } else {
